01830b2fdc6c79be036b050979a4976783b1d492
Kedar Abhyankar <fishyloginsystem@gmail.com>
2017-11-09 17:55:37 -0800

    added bigger font sizes

--- scrips.js
+++ scrips.js
@@ -4,7 +4,7 @@ function basicInfoFunction()
   firebase.auth().onAuthStateChanged(function(user) {
     if (user && user.uid != currentUid) {
       currentUid = user.uid;
-      document.getElementById("basicInfo").innerHTML = 'Full Name: ' + user.displayName + '<br> Email: ' + user.email + '<br>';
+      document.getElementById("basicInfo").innerHTML = '<font size="5"> Full Name: ' + user.displayName + '<br> Email: ' + user.email + '<br></font>';
     } else {
       currentUid = null;
       console.log("no user signed in");
