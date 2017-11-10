d9f0d8f889d4d7421f858cac5d208f7f40f5c852
Kedar Abhyankar <fishyloginsystem@gmail.com>
2017-11-09 17:57:59 -0800

    added strong lmao

--- scrips.js
+++ scrips.js
@@ -4,7 +4,7 @@ function basicInfoFunction()
   firebase.auth().onAuthStateChanged(function(user) {
     if (user && user.uid != currentUid) {
       currentUid = user.uid;
-      document.getElementById("basicInfo").innerHTML = '<font size="5"> Full Name: ' + user.displayName + '<br> Email: ' + user.email + '<br></font>';
+      document.getElementById("basicInfo").innerHTML = '<strong><font size="5"> Full Name: </strong>' + user.displayName + '<br> <strong>Email: </strong>' + user.email + '<br></font>';
     } else {
       currentUid = null;
       console.log("no user signed in");
